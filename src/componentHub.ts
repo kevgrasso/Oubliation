568bdcb8eb66aa0d20fabd589b758872cccc247c
Kevin Grasso <kevgrasso@gmail.com>
2016-06-14 21:06:41 -0400

    rewrote components, changed utilities to better support lodash

--- src/componentHub.ts
+++ src/componentHub.ts
@@ -1,176 +1,73 @@
-// TODO: consider ComponentModule class
-// TODO: store binded methods and nodes together
+type ComponentSpec = [new (...args: any[]) => ComponentNode, any[]]
 
 interface ComponentMethodData<T extends Function> {
-       origin: ComponentNode
-       source: T
-       method: T
-       priority: number
-} 
-
-class ComponentMethodModule<T extends Function> {
-    public fire: T
-    protected methods: T[]
-    private methodData: ComponentMethodData<T>[]
-    
-    // HACK: in 2.0, use specified this types so fire method can be described in constructor call & subclassing is not required
-    // HACK: in 2.1, use vararg types to describe fire method
-    constructor() {
-        this.methodData = []
-        this.methods = []
-    }
-    
-    public add(componentNode: ComponentNode, method: T) {
-        if (!_.includes<T>(Object.values(componentNode), method)) {
-            throw new Error(`Function ${method} not found in ComponentNode ${componentNode}`)
-        }
-        
-        let entry = {
-            origin: componentNode,
-            source: method,
-            method: _.bind<T, T>(method, componentNode),
-            priority: componentNode.getPriority()
-        }
-        
-        // NOTE: does not guarantee uniqueness
-        Utils.sortedInsertBy(this.methodData, entry, 'priority')
-        this.methods = _.map<ComponentMethodData<T>, T>(this.methodData, 'method')
-        
-    }
-    
-    public remove(componentNode: ComponentNode, method: T) {
-        if (!_.includes<T>(Object.values(componentNode), method)) {
-            throw new Error(`Function ${method} not found in ComponentNode ${componentNode}`)
-        }
-        let result = _.remove(this.methodData, (data: ComponentMethodData<T>) => {
-            return data.origin === componentNode && data.source === method
-        })
-        
-        if (result.length === 0) {
-            throw new Error(`Function ${method} of ComponentNode ${componentNode} not found in module`)
-        }
-        this.methods = _.map<ComponentMethodData<T>, T>(this.methodData, 'method')
-    }
+    method: T
+    priority: number
 }
-
-class TestModule extends ComponentMethodModule<() => void> {
-    public fire: ( () => void ) = () => {
-        // ONLY FOR TESTING
-        return this.methods
-    }
-}
-
-class ComponentHub {
-    private componentKinds: {[kind: string]: Set<ComponentPack>}
-    private componentPacks: Map<ComponentPack, ComponentNode[]>
-    private componentMethods: {[method: string]: ComponentMethodModule<Function>}
+const Components = {
+    extractMethods<T extends Function>(methodData: ComponentMethodData<T>[]): T[] {
+        return _.map<ComponentMethodData<T>, T>(methodData, 'method')
+    },
     
-    constructor(private hubSpec: {[kind: string]: number}, ...componentSpecs: [new (...args: any[]) => ComponentPack, any[]][]) {
-        for (const kind of Object.getOwnPropertyNames(hubSpec)) {
-            this.componentKinds[kind] = new Set<ComponentPack>()
-        }
-        
-        for (const componentSpec of componentSpecs) {
-            this.addPack(new componentSpec[0](this, ...componentSpec[1]))
-        }
-    }
-    
-    public getNumSlotsOpen(kind: string) {
-        return this.hubSpec[kind] - this.componentKinds[kind].size
-    }
-    
-    public hasPack(componentPack: ComponentPack) {
-        const kind = componentPack.getKind()
-        return this.componentKinds[kind].has(componentPack)
-    }
-    
-    public addPack(componentPack: ComponentPack) {
-        const kind = componentPack.getKind()
-        if (this.getNumSlotsOpen(kind) > 0) {
-            const componentNodes = componentPack.getComponents(this)
-            
-            // insert pack into kinds table
-            this.componentKinds[kind].add(componentPack)
-            // insert nodes into packs table
-            this.componentPacks.set(componentPack, componentNodes)
-            
-            const componentMethods = this.componentMethods
-            for (const componentNode of componentNodes) {
-                const priority = componentNode.getPriority()
-                for (const componentMethod of componentNode.getMethodNames()) {
-                    // TODO: handle nulls properly
-                    componentMethods[componentMethod].add(componentNode, _.get<Function>(componentNode, 'getPriority'))
-                }
-                
-                
-            }
-        }
-    }
-    
-    public removePack(componentPack: ComponentPack) {
-        this.componentKinds[componentPack.getKind()].delete(componentPack)
-        this.componentPacks.delete(componentPack)
-        
-        const componentNodes = componentPack.getComponents(this)
-        const componentMethods = this.componentMethods
+    insertMethodData<T extends Function>(name: string, componentNodes: ComponentNode[], result: ComponentMethodData<T>[]): ComponentMethodData<T>[] {
         for (const componentNode of componentNodes) {
-            const priority = componentNode.getPriority()
-            for (const componentMethod of componentNode.getMethodNames()) {
-                // TODO: handle nulls properly
-                componentMethods[componentMethod].remove(componentNode, _.get<Function>(componentNode, 'getPriority'))
+            const method = _.get<ComponentNode, T>(componentNode, name)
+            // only insert retrieved methods that aren't the invalid defaults 
+            if (method !== _.get<ComponentMethods, T>(ComponentMethods.prototype, name)) { 
+                const methodData: ComponentMethodData<T> = {
+                    method: _.bind<T, T>(method, componentNode),
+                    priority: componentNode.getPriority()
+                }
+                Utils.sortedInsertBy(methodData, result, 'priority')
             }
         }
+        return result
     }
 }
 
-interface ComponentSpec {
-    class: new (...args: any[]) => ComponentNode
-    args: any[]
-}
+abstract class ComponentHub {
+    private componentNodes: ComponentNode[]
 
-class ComponentPack {
-    private componentSpecs: {
-        class: new (...args: any[]) => ComponentNode
-        args: any[]
-    }[]
-        
-    constructor(private kind: string, ...componentSpecs: ComponentSpec[]) {
-        this.componentSpecs = componentSpecs
-    }
-    
-    public getKind() {
-        return this.kind
-    }
-    
-    public getComponents(owner: ComponentHub): ComponentNode[] {
-        return _.map(this.componentSpecs, (componentSpec: ComponentSpec) => {
-            return new componentSpec.class(owner, ...componentSpec.args)
+    constructor(...componentSpecs: ComponentSpec[]) {
+        this.componentNodes = _.map<ComponentSpec, ComponentNode>(componentSpecs, (componentSpec: [new (...args: any[]) => ComponentNode, any[]]): ComponentNode => {
+            // what's wrong with this coloring??
+            return new componentSpec[0](this, ...componentSpec[1])
         })
     }
+
+    public getAllMethodData<T extends Function>(name: string, result: ComponentMethodData<T>[]): ComponentMethodData<T>[] {
+        return this.getDirectMethodData<T>(name, this.getIndirectMethodData<T>(name, []))
+    }
+
+    protected getAllMethods<T extends Function>(name: string): T[] {
+        return Components.extractMethods<T>(this.getAllMethodData<T>(name, []))
+    }
+
+    private getDirectMethodData<T extends Function>(name: string, result: ComponentMethodData<T>[]): ComponentMethodData<T>[] {
+        return Components.insertMethodData(name, this.componentNodes, result)
+    }
+
+    private getIndirectMethodData<T extends Function>(name: string, result: ComponentMethodData<T>[]): ComponentMethodData<T>[] {
+        for (const retrieverData of this.getDirectMethodData<(name: string, result: ComponentMethodData<T>[]) => ComponentMethodData<T>[]>('getIndirectMethodData', [])) {
+            retrieverData.method(name, result)
+        }
+        return result
+    }
+
 }
 
-abstract class ComponentNode {
+abstract class ComponentNode extends ComponentMethods {
     constructor(
         private owner: ComponentHub,
         private priority: number,
         ...args: any[]  // HACK: get rid of this in later Typescript version if possible
     ) {
+        super()
         if (args.length !== 0) {
             throw new Error('ComponentNode should not be called with arguments besides owner and priority')
         }
     }
-    
-    public getMethodNames() {
-        const blacklist: string[] = []
-        return _.difference(
-            _.union(
-                _.functionsIn(this),
-                _.functionsIn(ComponentHub.prototype)
-            ),
-            blacklist
-        )
-    }
-    
+
     public getOwner() {
         return this.owner
     }
--- src/utilities.ts
+++ src/utilities.ts
@@ -1,17 +1,17 @@
 const Utils = {
-    sortedInsert<T>(array: T[], element: T) {
+    sortedInsert<T>(element: T, array: T[]) {
         array.splice(_.sortedIndex(array, element), 0, element)
     },
     
-    sortedInsertBy<T>(array: T[], element: T, iteratee: any) {
+    sortedInsertBy<T>(element: T, array: T[], iteratee: any) {
         array.splice(_.sortedIndexBy(array, element, iteratee), 0, element)
     },
     
-    remove<T>(array: T[], element: T) {
+    remove<T>(element: T, array: T[]) {
         array.splice(_.indexOf(array, element), 1)
     },
     
-    sortedRemove<T>(array: T[], element: T) {
+    sortedRemove<T>(element: T, array: T[]) {
         array.splice(_.sortedIndexOf(array, element), 1)
     }
 }
