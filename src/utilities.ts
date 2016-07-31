b0663999d47a0aa11ce88e550ffeb5d31509e1bb
Kevin Grasso <kevgrasso@gmail.com>
2016-07-31 18:01:58 -0400

    Utilities refactored

--- src/utilities.ts
+++ src/utilities.ts
@@ -1,17 +1,19 @@
-const Utils = {
-    sortedInsert<T>(element: T, array: T[]) {
-        array.splice(_.sortedIndex(array, element), 0, element)
-    },
+namespace Utils {
+    'use strict';
     
-    sortedInsertBy<T>(element: T, array: T[], iteratee: any) {
-        array.splice(_.sortedIndexBy(array, element, iteratee), 0, element)
-    },
-    
-    remove<T>(element: T, array: T[]) {
-        array.splice(_.indexOf(array, element), 1)
-    },
-    
-    sortedRemove<T>(element: T, array: T[]) {
-        array.splice(_.sortedIndexOf(array, element), 1)
+    export function sortedInsert<T>(element: T, array: T[]) {
+        array.splice(_.sortedIndex(array, element), 0, element);
     }
-}
+    
+    export function sortedInsertBy<T>(element: T, array: T[], iteratee: any) {
+        array.splice(_.sortedIndexBy(array, element, iteratee), 0, element);
+    }
+    
+    export function remove<T>(element: T, array: T[]) {
+        array.splice(_.indexOf(array, element), 1);
+    }
+    
+    export function sortedRemove<T>(element: T, array: T[]) {
+        array.splice(_.sortedIndexOf(array, element), 1);
+    }
+};
